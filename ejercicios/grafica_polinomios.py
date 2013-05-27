c41b9ac9b0d0c7ae97b2003101f73dc83e87b859
Jeudy <jeudy.blanco@cinespa.ucr.ac.cr>
2013-05-27 00:50:18 -0600

    Ejercicio de graficacion simple de polinomios

---
+++ ejercicios/grafica_polinomios.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = np.poly1d([6, 9, 0, -2])
+
+df = f.deriv()
+
+x = np.linspace(-2, 2, 1000)
+
+y = f(x)
+
+dy = df(x)
+
+fig = plt.figure("Graficando polinomios")
+
+sp1 = fig.add_subplot(121, title='$f(x) = 6x^3 + 9x^2 - 2$')
+plt.xticks([-2, -1, 0, 1, 2])
+sp2 = fig.add_subplot(122, title='$df(x)$')
+sp1.plot(x, y, color="red")
+
+plt.xticks([-2, -1, 0, 1, 2])
+
+sp2.plot(x, dy, color="blue")
+
+plt.show()
